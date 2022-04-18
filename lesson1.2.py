e57a82d0ec1d0e9b9099098cfaa37f4ffd72372b
e.luneva <e.luneva@tinkoff.ru>
2022-04-18 18:49:19 +0300

    lesson1.2

---
+++ lesson1.2.py
@@ -0,0 +1,12 @@
+#Узнайте у пользователя число n.
+#Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+
+number input('enter the nuber from 1 to 9')
+#if number > 9 or number < 1:
+#    print ('error: the number is not correct')
+#else:
+num_2 = number * 2
+num_3 = number * 3
+print (num_3, num_2)
+
+
